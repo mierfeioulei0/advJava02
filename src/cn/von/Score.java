3c745178fa44e511389aba82296a5f532996aba4
mierfeioulei0 <371696999@qq.com>
2018-10-05 13:05:07 +0800

    -ver1.0 -修改分数类

--- src/cn/von/Score.java
+++ src/cn/von/Score.java
@@ -1,13 +1,26 @@
 package cn.von;
 
+import lombok.Data;
+
 /**
- * Created by mierfeioulei0 on 2018/9/26.
+ * 分数类
+ *
+ * @author mierfeioulei0
+ * @version 1.0
+ * @since 2018/10/05
  */
+@Data
 public class Score {
-    String courseName;
-    float courseScore;
-    Score(String courseName,float courseScore){
-        this.courseName=courseName;
-        this.courseScore=courseScore;
+    private String courseName;
+    private double courseScore;
+
+    Score() {
+
     }
+
+    Score(String courseName, double courseScore) {
+        this.courseName = courseName;
+        this.courseScore = courseScore;
+    }
+
 }
