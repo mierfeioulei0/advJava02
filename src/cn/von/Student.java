77f3e4d1b3aa702e3818159e431c41807e9ef691
mierfeioulei0 <371696999@qq.com>
2018-10-05 12:59:56 +0800

    -ver1.0 -修改学生类

--- src/cn/von/Student.java
+++ src/cn/von/Student.java
@@ -1,20 +1,44 @@
 package cn.von;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by mierfeioulei0 on 2018/9/26.
+ * 学生类
+ *
+ * @author mierfeioulei0
+ * @version 1.0
+ * @since 2018/10/05
  */
+@Data
 public class Student {
-    String id;
-    String name;
-    ArrayList<Score> scoreList;
-    Student(String id, String name, List<Score> scoreList){
-        this.id=id;
+    private String id;//学号
+    private String name;//姓名
+    private ArrayList<Score> scoreList;//成绩列表
+
+    Student() {
+
+    }
+
+    Student(String id, String name, List<Score> scoreList) {
+        this.id = id;
         this.name = name;
+        //深拷贝
         this.scoreList = new ArrayList<>();
         this.scoreList.addAll(scoreList);//
         //scoreList.forEach(s->this.scoreList.add(s));//
+    }
+
+    /**
+     * 成绩列表深拷贝
+     *
+     * @author mierfeioulei0
+     * @param scoreList
+     */
+    public void setScoreList(ArrayList<Score> scoreList) {
+        this.scoreList = new ArrayList<>();
+        this.scoreList.addAll(scoreList);
     }
 }
